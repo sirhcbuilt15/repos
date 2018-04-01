6b7aa1282cfbb3f366073cf47a1b6a208ad1e5ae
Nomolas <nomolas.sirhc@gmail.com>
2018-04-01 18:51:58 -0500

    merge two, a code line change

--- repos/helloGit.h
+++ repos/helloGit.h
@@ -1,5 +1,5 @@
 // this is a file for function prototipes from HelloGit.cpp
 // Nothing to do so far
-
+// repeting the merge, only for purpose of learning
 // try to use cherry-pick. this is a third commit
 
