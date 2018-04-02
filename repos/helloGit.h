2c666e6aad054b2f0e3fd52687c95d4a2d427625
Nomolas <nomolas.sirhc@gmail.com>
2018-04-01 19:02:27 -0500

    The file is complete now. Check  the comments.

--- repos/helloGit.h
+++ repos/helloGit.h
@@ -1,8 +1,5 @@
 // this is a file for function prototipes from HelloGit.cpp
 // Nothing to do so far
-<<<<<<< HEAD
 // make an another commit
-=======
 // repeting the merge, only for purpose of learning
->>>>>>> Experimental
 // try to use cherry-pick. this is a third commit
